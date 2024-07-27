cbeec1be34e6fe0eb62ce9e8c9645b47dddb78f9
TotallyNotKonnan <brovnabrovanna@gmail.com>
2024-07-27 09:07:20 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -14,7 +14,7 @@ local Window = Fluent:CreateWindow({
 
 --Fluent provides Lucide Icons https://lucide.dev/icons/ for the tabs, icons are optional
 local Tabs = {
-    Main = Window:AddTab({ Title = "Main", Icon = "library" }),
+    Main = Window:AddTab({ Title = "Main", Icon = "menu" }),
     Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
 }
 
